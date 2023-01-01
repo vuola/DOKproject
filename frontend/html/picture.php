5e0051706600bcdb44664edde3436da3b85c4f2d
vuola <markus.vuolahti@gmail.com>
2023-01-01 10:12:58 +0200

    debug3

--- frontend/html/picture.php
+++ frontend/html/picture.php
@@ -1,25 +1,27 @@
 <?php
   
 $url = 'https://picsum.photos/1200'; 
-$img = '/tmp/kube/image.gif'; 
+$img = '/tmp/kube/image.gif';
+$context = stream_context_create(
+    array(
+        "http" => array(
+            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+        )
+    )
+);     
 
 if (file_exists($img)) {
     $date1 = date("d/m/Y", filemtime($img)); // date of file
     $date2 = date("d/m/Y"); // today
     if ($date1 < $date2) {
         // Function to write image into file
-        file_put_contents($img, file_get_contents($url));
+        $content = file_get_contents($url, false, $context);
+        file_put_contents($img, $content);
     }    
 }
 else {
     // No file, retrieve one
-    $context = stream_context_create(
-        array(
-            "http" => array(
-                "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
-            )
-        )
-    );    
+ 
     $content = file_get_contents($url, false, $context);
     file_put_contents($img, $content);
 }
